fa886fef7a14ac828e44f2fac1d9d25eb9607e0a
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-10-29 22:58:59 -0300

    feat: change list reqs to actual db

--- src/routes/completed/+page.server.ts
+++ src/routes/completed/+page.server.ts
@@ -1,17 +1,21 @@
-import { recommendations as db, markAsNotCompleted } from '$lib/data';
+import { markAsNotCompleted } from "$lib/data";
+import RecomendationService from "../../services/RecomendationService.js";
 
-export function load() {
+export async function load() {
+  const recommendations = await RecomendationService.getRecomendations();
   return {
-    recommendations: db.filter((recommendation) => recommendation.completed)
-  }
+    recommendations: recommendations.filter(
+      (recommendation) => recommendation.completed
+    ),
+  };
 }
 
 export const actions = {
-  markAsCompleted: async ({request}) => {
-    const data = await request.formData()
-    const title = data.get('recommendation')
-    if(title) {
-      markAsNotCompleted(title)
+  markAsCompleted: async ({ request }) => {
+    const data = await request.formData();
+    const title = data.get("recommendation");
+    if (title) {
+      markAsNotCompleted(title);
     }
-  }
-}
+  },
+};
