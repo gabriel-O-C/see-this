9e86638aedbea4be7afd6eca230dd66fd7e6f7e0
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-10-30 16:30:01 -0300

    feat: add view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,29 @@
 <script>
+  import { onNavigate } from "$app/navigation";
   import "../global.css";
-</script>
-<div class="h-screen p-2 text-white bg-slate-800">
 
+  onNavigate((navigation) => {
+    //@ts-expect-error this api types doesnt exist yet
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+      //@ts-expect-error this api types doesnt exist yet
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
+</script>
+
+<div class="h-screen p-2 text-white bg-slate-800">
   <nav class="p-2">
     <ul class="flex flex-col items-start gap-2 mb-4 md:flex-row">
       <li class="hover:underline"><a href="/">home</a></li>
       <li class="hover:underline"><a href="/completed">completed</a></li>
-      <li class="hover:underline"><a href="/new-sugestion">new sugestion</a></li>
+      <li class="hover:underline">
+        <a href="/new-sugestion">new sugestion</a>
+      </li>
     </ul>
   </nav>
   <slot />
